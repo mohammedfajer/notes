1c203d0532428686c0d91e1dc52989ea31f063b5
mohammedfajer <mohammedfajer11@gmail.com>
2023-09-23 21:13:45 +0100

    Updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,8 +58,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/mohammedfajer/notes',          
+         /*  editUrl:
+            'https://github.com/mohammedfajer/notes',    */       
         
             remarkPlugins: [math],
             rehypePlugins: [katex],
